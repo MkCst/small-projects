a70f9947cc8d9b4e88bb9c0a1e85f301026ca8dd
MkCst <mikelopezcastillo@gmail.com>
2020-11-29 17:40:01 -0600

    Se corrigen errores, se agrega archivo .gitignore, se agregan las librerias del ambiente

--- principal.py
+++ principal.py
@@ -1,11 +1,9 @@
-
-from random import randint
 from borracho import BorrachoTradicional
 from campo import Campo
-from coordenada import Coordenada
+from coordenada import  Coordenada
 from bokeh.plotting import figure, show, output_file
-# Metodos que serviran para modelar en la clase principal
 
+# Metodos que serviran para modelar en la clase principal
 def caminata(campo, borracho, pasos):
     """obtiene toda la caminata y la distancia total """
     inicio = campo.obtener_coordenadas(borracho)
@@ -21,7 +19,7 @@ def simular_caminata(pasos, intentos, BorrachoTradicional):
     
     for _ in range(intentos):
         campo = Campo()
-        campo.add_borrachos(borracho, origen)
+        campo.add_borracho(borracho, origen)
         simulacion_caminata = caminata(campo, borracho, pasos)
         distancias.append(round(simulacion_caminata,1))
     return distancias
@@ -38,23 +36,25 @@ def graficar(x, y):
 
 if __name__ == "__main__":
     print("hola bienvenido!")
-    distancia_por_caminata = []
-    distancias_caminata = [10,100,1000]
+    distancia_recorrida = []
+    # Pasos a caminar
+    pasos_totales= [10,100,1000]
     intentos = 10
     
-    for pasos in distancia_por_caminata:
-        distancias  = simular_caminata(pasos, intentos,BorrachoTradicional ) 
+    for pasos in pasos_totales:
+        distancias  = simular_caminata(pasos, intentos, BorrachoTradicional ) 
         distancia_media = round(sum(distancias)/len(distancias), 4)
         distancia_maxima = max(distancias)
         distancia_minima = min(distancias)
         
-        distancia_por_caminata.append(distancia_media)
+        distancia_recorrida.append(distancia_media)
         
+        print("Distancia recorrida total en pasos: ")
         print(f"{BorrachoTradicional.__name__} caminata aleatoria de {pasos} pasos")
         print(f"Media: {distancia_media}")
         print(f"Minima: {distancia_minima}")
         print(f"Maxima: {distancia_maxima}")
         print()
-        graficar(distancias_caminata, distancia_por_caminata)
+    graficar(pasos_totales, distancia_recorrida)
     print("adios")
     
