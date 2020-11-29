1bf6eb4e4c86060c999325c04f85f6f45f62b655
MkCst <mikelopezcastillo@gmail.com>
2020-11-28 20:28:39 -0600

    Primer commit, se agregan las clases borracho y coordenada

---
+++ borracho.py
@@ -0,0 +1,12 @@
+from random import choice
+
+class Borracho:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    
+class BorrachoTradicional(Borracho):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+    
+    def camina(self):
+        return choice([(0,1),(1,0),(-1,0),(0,-1)])
